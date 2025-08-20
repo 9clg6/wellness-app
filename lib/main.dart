2f8777b2710ad83007ed9153e4b84a3c95b45bc7
Clement Guyon <clement.guyon@orkester.fr>
2025-08-20 22:54:52 +0200

    feat: preserve splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:starter_kit/presentation/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   const Locale fr = Locale('fr');
   const Locale en = Locale('en');
