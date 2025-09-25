cbc4c2746881e951ce35a5b9b28032ab743e61f9
Clement Guyon <clement.guyon@orkester.fr>
2025-09-25 11:17:16 +0200

    feat: working sign in with apple

--- lib/presentation/screens/authentication/authentication.screen.dart
+++ lib/presentation/screens/authentication/authentication.screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:welly/presentation/screens/authentication/authentication.state.dart';
 import 'package:welly/presentation/screens/authentication/authentication.view_model.dart';
 import 'package:welly/presentation/widgets/custom_loader.dart';
@@ -34,9 +37,13 @@ class _AuthenticationScreenState extends ConsumerState<AuthenticationScreen> {
           child: state.when(
             data: (AuthenticationState data) => Form(
               key: viewModel.formKey,
-              child: const Column(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[Gap(96)],
+                children: <Widget>[
+                  const Gap(96),
+                  if (Platform.isIOS)
+                    SignInWithAppleButton(onPressed: viewModel.loginWithApple),
+                ],
               ),
             ),
             error: ErrorPlaceholder.new,
