a2443697be45ab14f46eff6c5b31e98c9f6a5ab0
Clement Guyon <clement.guyon@orkester.fr>
2025-09-25 21:02:45 +0200

    feat: working sign in with google on android

---
+++ lib/core/providers/core/services/auth_state.provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_state.provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authStateHash() => r'e373767a2056a644e7139a513b9a3eb6322d2a0a';
+
+/// Provider that streams Firebase authentication state changes
+///
+/// Copied from [authState].
+@ProviderFor(authState)
+final authStateProvider = AutoDisposeStreamProvider<User?>.internal(
+  authState,
+  name: r'authStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthStateRef = AutoDisposeStreamProviderRef<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
